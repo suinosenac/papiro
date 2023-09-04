069900679bf6f13e1385e0ac949923df00d7b105
suinosenac <143208992+suinosenac@users.noreply.github.com>
2023-09-04 14:57:19 -0300

    alterações do dia 04/09/2023

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// function validar() {
+
+//     const senha = document.getElementById("senha").value;
+//     const user = document.getElementById("user").value;
+
+//     if (!senha) {
+//         alert('Senha inválida');
+//     } else if (!validarEmail(senha)) {
+//         alert('Senha inválida');
+//     }
+
+//     if (!user) {
+//         alert('Usuário inválido');
+//     } else if (!validarUsername(user)) {
+//         alert('Usuário inválido');
+//     }
+ 
+// }
